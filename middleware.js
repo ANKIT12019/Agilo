76c88db1e3e9fc6172ecc288306fce8c4111b588
ANKIT12019 <rajankit12019@gmail.com>
2025-04-06 23:57:30 +0530

    Initial commit

--- middleware.js
+++ middleware.js
@@ -1,5 +1,7 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
+import { authMiddleware } from "@clerk/nextjs/server";
+
 
 const isProtectedRoute = createRouteMatcher([
   "/onboarding(.*)",
